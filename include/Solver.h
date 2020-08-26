928513e76325dd6f56ef4571c64b1b5dccae7e13
Piotr Futymski <piotr.futymski@wp.pl>
2020-08-26 10:57:27 +0200

    back to ealier implementation

--- include/Solver.h
+++ include/Solver.h
@@ -48,35 +48,23 @@ int max_iter = 100, double err = 1E-10, double omega = 1.0)
 
     auto omegaI = interval_arithmetic::Interval<double>(omega);
 
-    int itNum = 0;
+     int itNum = 0;
     auto sol = start_solution;
     while(itNum < max_iter)
-    { 
-        bool tobreak = true;     
-
-        auto solleft = sol;
-        auto solright = sol;
-
-        for (int i = 0; i < sol.size(); i++)
-        {
-            solleft[i] = sol[i].a;
-            solright[i] = sol[i].b;
-        }
-
+    {
+        auto mval = sol;
+        bool tobreak = true;
         for (int i = 0; i < sol.size(); i++)
         {
             try{
-                auto a = solleft[i] - omegaI * fi_x(solleft, i) / dfi_x(solleft,i);
-                auto b = solright[i] - omegaI * fi_x(solright, i) / dfi_x(solright,i);
-
-                sol[i].a = std::min(a.a, b.a);
-                sol[i].b = std::max(a.b,b.b);
+                sol[i] = mval[i] - omegaI * fi_x(sol,i) / dfi_x(sol,i); 
             }
             catch (std::exception & e)
             {
                     return 2;            
             }
-            if(sol[i].b - sol[i].a > err)
+            if((sol[i].a - mval[i].a > err || sol[i].a-mval[i].a < -1 * err) ||
+                (sol[i].b - mval[i].b > err || sol[i].b-mval[i].b < -1 * err))
                 tobreak = false;
             if(isnan(sol[i].a) || isnan(sol[i].b))
                 return 2;
@@ -94,6 +82,6 @@ int max_iter = 100, double err = 1E-10, double omega = 1.0)
         return 0;
 
     if(itNum >= max_iter)
-        return 1;
+        return 1;  
     return -1;
 }
